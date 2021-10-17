b7d0c74d6505422d6bc82463841163e55994b3f9
rbouchard13 <88938831+rbouchard13@users.noreply.github.com>
2021-10-17 15:25:33 -0400

    Add files via upload

---
+++ mapanimation.js
@@ -0,0 +1,76 @@
+var map;
+var markers = [];
+var url;
+var tmout;
+function updateUrl(){
+	markers.forEach((item) => {item.remove();});
+	markers = [];
+	clearTimeout(tmout);
+	addMarkers();
+}
+function init(){
+//url = 'https://api-v3.mbta.com/vehicles?&include=trip'; //place before include to set specific route    filter[route]=1&
+mapboxgl.accessToken = 'pk.eyJ1IjoidGhlZGFkYXMxMzEzIiwiYSI6ImNrdXNrOXdwbTB3M2Uybm82d2V1bXljbjgifQ.Qk2kDT-hQODQFqGghcr4lQ';
+	var element = document.getElementById('map');
+	map = new mapboxgl.Map({
+  		container: 'map',
+  		style: 'mapbox://styles/mapbox/dark-v10',
+  		center: [-71.104081, 42.365554],
+  		zoom: 14,
+	});
+  	addMarkers();
+}
+async function addMarkers(){
+	var route = document.getElementById("routes").value;
+	if (route === "")
+		{url = 'https://api-v3.mbta.com/vehicles?&include=trip';}
+	else
+		{url = 'https://api-v3.mbta.com/vehicles?&filter[route]=' + route + '&include=trip';}	
+	var locations = await getBusLocations();
+	locations.forEach(function(bus){
+		var marker = getMarker(bus.id);		
+		if (marker){
+			moveMarker(marker,bus);
+		}
+		else{
+			addMarker(bus);			
+		}
+	});
+	console.log(new Date());
+	tmout = setTimeout(addMarkers,15000);
+}
+
+async function getBusLocations(){		
+	var response = await fetch(url);
+	var json     = await response.json();
+	return json.data;
+}
+function addMarker(bus){
+	var icon = getIcon(bus);
+	var marker = new mapboxgl.Marker({
+		color: icon
+	})
+	marker.setLngLat([bus.attributes.longitude, bus.attributes.latitude]);
+	marker._element.id = bus.id;
+	marker.addTo(map);
+	markers.push(marker);
+}
+function getIcon(bus){
+	if (bus.attributes.direction_id === 0) {
+		return '#f54842';
+	}
+	return '#18fc03';	
+}
+function moveMarker(marker,bus) {
+	var icon =  getIcon(bus);
+	marker._color = icon;
+	marker.setLngLat([bus.attributes.longitude, bus.attributes.latitude]);	
+}
+function getMarker(id){
+	var marker = markers.find(function(item){
+		return item._element.id === id;
+	});
+	return marker;
+}
+
+window.onload = init;
