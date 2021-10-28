d3a70b68334bb70662bf32c7b14994870c233ee0
rbouchard13 <88938831+rbouchard13@users.noreply.github.com>
2021-10-28 18:15:08 -0400

    Update Port.js

--- Port.js
+++ Port.js
@@ -28,18 +28,3 @@ function show(div){
 function ckMenu() {
 	if (document.getElementById("mySidenav").style.width = "200px"){closeNav();}
 }
-
-
-//	Eye Movement Animation	*******************************************************
-
-var balls = document.getElementsByClassName("ball");
-document.onmousemove = () => {
-      var x = (event.clientX * 100) / window.innerWidth + "%";
-      var y = (event.clientY * 100) / window.innerHeight + "%";
-
-      for (let i = 0; i < 2; i++) {
-        balls[i].style.left = x;
-        balls[i].style.top = y;
-        balls[i].transform = "translate(-" + x + ",-" + y + ")";
-      }
- };
