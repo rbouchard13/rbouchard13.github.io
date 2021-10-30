82b49590de8ee0c15d1954507c2d9aa7b3f1d02e
rbouchard13 <88938831+rbouchard13@users.noreply.github.com>
2021-10-30 18:58:09 -0400

    Update Port.js

--- Port.js
+++ Port.js
@@ -1,15 +1,9 @@
 function openNav() {
   	document.getElementById("mySidenav").style.width = "200px";
-  	document.getElementById("main").style.marginLeft = "200px";
-	document.getElementById("contact").style.marginLeft = "200px";
-	document.getElementById("projects").style.marginLeft = "200px";
 }
 
 function closeNav() {
   	document.getElementById("mySidenav").style.width = "0";
-  	document.getElementById("main").style.marginLeft = "0";
-	document.getElementById("contact").style.marginLeft = "0";
-	document.getElementById("projects").style.marginLeft = "0";
 }
 
 function show(div){
